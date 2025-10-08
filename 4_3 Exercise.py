80679f463ca6893b7ff74bf07c19448668f55198
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-10-08 16:41:22 +0530

    Create 4_3 Exercise.py

---
+++ 4_3 Exercise.py
@@ -0,0 +1,9 @@
+def get_input():
+    # Only edit the code segment between the dashes
+    
+    # ------------------------------------
+    a = int(input("Enter number one: "))
+    b = int(input("Enter number two: "))
+    # ------------------------------------
+    
+    print(a + b)
