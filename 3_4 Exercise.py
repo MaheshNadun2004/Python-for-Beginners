28ee758e153c305c57f8fd7e647b1fdb50715025
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-10-08 16:37:45 +0530

    Create 3_4 Exercise.py

---
+++ 3_4 Exercise.py
@@ -0,0 +1,12 @@
+X = int(input()) 
+
+Y = 4 * X + 14
+
+#--------TYPE YOUR CODE TO COMPLETE PROGRAM BELOW THIS LINE----------
+if Y > 50:
+    Z = Y + 4
+else:
+    Z = Y * 2
+#--------TYPE YOUR CODE TO COMPLETE PROGRAM ABOVE THIS LINE----------
+
+print(Z)
